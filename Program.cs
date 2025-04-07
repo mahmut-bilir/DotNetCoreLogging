437357c2a7285946e333e7ca0fe8714552d769a2
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-04-07 12:11:54 +0300

    Serilog usage

--- Program.cs
+++ Program.cs
@@ -1,18 +1,32 @@
 ﻿
 
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 
-using var loggerFactory = LoggerFactory.Create(builder =>
-{
+// using var loggerFactory = LoggerFactory.Create(builder =>
+// {
 
-    builder.AddFilter("Default", LogLevel.Information)
-  .AddConsole();
+//     builder.AddFilter("Default", LogLevel.Information)
+//   .AddConsole();
 
-});
+// });
 
-ILogger logger = loggerFactory.CreateLogger<Program>();
+// ILogger logger = loggerFactory.CreateLogger<Program>();
 
-logger.LogInformation("Info log..");
-logger.LogWarning("Warning log..");
-logger.LogError("Error log..");
+// logger.LogInformation("Info log..");
+// logger.LogWarning("Warning log..");
+// logger.LogError("Error log..");
+
+
+Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Information()
+.WriteTo.Console()
+.WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)
+.CreateLogger();
+
+Log.Information("Serilog info");
+Log.Warning("Serilog warning");
+Log.Error("Serilog error");
+
+Log.CloseAndFlush();
