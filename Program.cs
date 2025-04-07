92daadcaf609d574ca43bf818b6d04c197676183
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-04-07 11:07:49 +0300

    Microsoft Extensions Log Example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Microsoft.Extensions.Logging;
+
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+
+    builder.AddFilter("Default", LogLevel.Information)
+  .AddConsole();
+
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("Info log..");
+logger.LogWarning("Warning log..");
+logger.LogError("Error log..");
